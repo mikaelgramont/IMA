f036f0656b24d6227a376d57674f253e56f8efdf
Mikael Gramont <mgramont@gmail.com>
2018-06-04 21:54:29 +0200

    Adding Mark's interview

---
+++ pages/interviews/5-mark-adams.php
@@ -0,0 +1,69 @@
+<!-- META -->
+<meta property="og:title" content="Getting to know... Mark Adams"/>
+<meta property="og:description" content="An IMA interview of the British mountainboarder. How many 16-year-olds do you know that have landed a double backflip on a mountainboard? Trick question, there’s only one and his name is Mark Adams"/>
+
+SEPARATOR
+
+<!-- SNIPPET -->
+
+<h2 class="display-font">Getting to know... Mark Adams</h2>
+<p>An IMA interview of the British mountainboarder.</p>
+
+SEPARATOR
+
+<!-- CONTENT -->
+
+<h1 class="display-font">Getting to know... Mark Adams</h1>
+
+<p>
+    How many 16-year-olds do you know that have landed a double backflip on a mountainboard?<br>
+    Trick question, there’s only one and his name is Mark Adams.<br>
+    Mark is 18 now and has been riding since he was 10. He has pushed the UK scene as an amateur and now is into the pro ranks. This is his story.
+</p>
+
+<p class="interview-image">
+    <img src="<?php echo BASE_URL ?>images/interviews/mark-1.jpg" alt="Mark Adams throwing a huge front flip">
+</p>
+
+<p>
+    As mentioned Mark Adams is the youngest athlete to double flip a mountainboard.
+    He grew up minutes from The Hales Board and Bike Park in Chester.
+    It’s a centre that many of the UK’s elite train at and it has given Mark a chance to ride with the world's best.
+    Having a foam pit, multiple jib lines, a BX track, jump line, and quarter pipe on site has aided in his progression.
+    You name it, Hale’s has it, and the people you’d want to be riding with.
+    There’s usually someone riding when you show up and the vibe around Hale’s is a lot of smiles and a lot of progression.
+</p>
+
+<p class="interview-image">
+    <img src="<?php echo BASE_URL ?>images/interviews/mark-2.jpg" alt="Mark Adams keeps going big">
+</p>
+
+<p>
+    This has helped push Mark to being one of the greatest junior riders in history.
+    He has won the under 16’s title in the UK and has podiumed in the pro category in one of the world’s most
+    competitive scenes. He loves front flips but has the big spins and flip combos as well.
+    He says that he’s impressed with how far tricks have progressed, since he started, with the help of foam pits and
+    training facilities. Which helps Mark because his favorite discipline is freestyle.
+</p>
+
+<p class="interview-image">
+    <img src="<?php echo BASE_URL ?>images/interviews/mark-3.jpg" alt="Mark Adams corking it out">
+</p>
+
+<p>
+    When asked about the scene in the UK, he says it's a great one and that athletes are always progressing although
+    he thinks better advertising and more people coming to ride could help the scene grow.
+    Otherwise getting together to ride more and creating memories is always helpful.
+    He looks back fondly on memories of throwing double backflips at the UK Championships and cementing himself in the
+    history books.
+</p>
+
+<div class="more-about">
+    More about Mark:
+    <ul>
+        <li>Instagram: <a href="https://www.instagram.com/mark783/">@mark783</a></li>
+        <li><a href="https://www.youtube.com/watch?v=VOYOCRWQ-vY">Mark's promo video</a></li>
+    </ul>
+</div>
+
+
