19fa142fd20cee87a98eb34874765d4449c829b7
Mikael Gramont <mgramont@gmail.com>
2019-01-06 15:17:10 +0100

    Adding Haero Boards interview

---
+++ pages/interviews/6-haero-boards.php
@@ -0,0 +1,190 @@
+<!-- META -->
+<meta property="og:title" content="Getting to know... Haero Boards"/>
+<meta property="og:description" content="An IMA interview of Haero Boards' founder, Phil Heinle."/>
+
+SEPARATOR
+
+<!-- SNIPPET -->
+
+<h2 class="display-font">Getting to know... Haero Boards</h2>
+<p>An IMA interview of Haero Boards' founder, Phil Heinle.</p>
+
+SEPARATOR
+
+<!-- CONTENT -->
+
+<h1 class="display-font">Getting to know... Haero Boards</h1>
+
+<p>
+  Mountainboarding has always been an evolving sport. It started with steel framed boards, boards with 20” rims, in-line boards, or even boards with 8 wheels! Once uni-body decks came out in 1999-2000 the progression of decks seemed to slow. Companies dialed in flex patterns and deck lengths. While trucks, wheels, and bindings took the forefront of progression. But in the last few years decks have seen an influx of technology. Concave re-entered the scene across the board and revolutionized the standard in mountainboarding. Rider owned companies popped up all over the world with each one making their dreams of a perfect mountainboard a reality.
+</p>
+
+<p>
+  One of the most anticipated iterations of these is from the German crew at Hero Boarding. They built what is arguably one of the most progressive mountainboards to date. Taking all of the best ideas over the years and combining them into one. Does concave, a p-tex base, and stiff construction pique your interest? It did ours, so we sat down with the mastermind of this project to see what the future holds for them.
+</p>
+
+<p class="question">
+  Mr Phil Heinle please introduce yourself.
+</p>
+<p>
+  Hey guys, im Phil from Germany and I am a mountainboard addict since 2009.
+  I was always into shaping my dreams and bring them to life. Starting with shaping my own dirts to building a World Championship track ending up with shaping the mountainboard deck we always dreamed of.
+</p>
+
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/6-haero/grosserlach.jpg" alt="Photo of the Grosserlach mountainboard center in Germany">
+</p>
+
+<p>Dreams can come true if you stick to them!</p>
+
+<p class="question">
+  How long have you been running “Hero Boarding”?
+</p>
+<p>
+  I think it was around 2012 when I took over the small Mountainboard Shop Hero Boarding wich started 1997 as a Kite Shop and got into Mountainboarding 2000-ish. First I was a team rider and supporter for them. As the old owner decided to stop running the shop because it wasn’t worth to run it further, I did not want this brand to die and left the market to ebay or non riders owned shops. So I offered to take over his business for saving the german community to be left with crappy boards and service. Surely it was nothing to get a millionaire :D
+</p>
+
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/6-haero/hero-teamrider.jpg" alt="Old photo of hero team rider Phil Heinle">
+</p>
+
+<p class="question">
+  So we’ve seen Joel Betzler riding a deck called the Bro-Model for a couple years now. It is definitely one of a kind. Tell us about how that came up and what makes it different from other decks on the market?
+</p>
+<p>
+  Yeah its almost 2 Years now Joel shredding our DIY Deck. Basically it was Joel forcing me to go that step.  I recognized his talent really fast and took him under contract as a team rider.
+</p>
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/6-haero/joel-start.jpg" alt="Joel Beltzer board sliding a rail">
+</p>
+<p>
+  After he was riding an old Kite-Landboard, he asked me to hook him up with the new concaved Decks which been the “latest thing” at that time. Unfortunately they´ve not been strong enough to handle his almost daily shredding in the Park. It ended up with monthly broken Decks. We always got replacement but he couldn't ride in the waiting time. I told him, wait for Colab to hit the market and ride the strong cap composite decks in the waiting time. But he was really impatient and was always asking, when are those new decks hitting the market? Kody, I think you remember me asking you remotely for the releasing date?! haha.
+</p>
+<p>
+  As I have some friends working in the snowboard and longboard industry, we've been confident enough to work on our very own vision of the best mountainboard deck together with their help. Funny Fact is, it was me chatting with TRAMPA to build a concaved deck back in Summer ´14. Unfortunately this never happened.
+</p>
+<p>
+  So what makes it different from other decks on the market? Hmm... In short, our Deck will be the same as others when it comes to Deck length and Truck angle but everything else is kind of different with some minor and some bigger changes. Is that enough?
+</p>
+
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/6-haero/deck1.jpg" alt="The BroModel deck">
+</p>
+
+<p class="question">
+  I think that doesn’t satisfy my and our followers expectations. Rumors say, that you are going to start producing decks for sale to the public. Please spill some of the secrets and tell us the features that will put this deck on the map as one of the best in the world.
+</p>
+<p>
+  Haha okay I knew you wanna know more details. The Rumors are true. Let's get into detail about the why first.<br>
+  As we built that first prototypes for us, it was never our intention to go public with them. But as <a href="https://www.instagram.com/p/BXvXvAEBPpO/?utm_source=ig_web_copy_link">we tested the first working prototypes with other riders</a>, we got really mind blowing feedback and many of them asking for the same deck.
+</p>
+<p>
+  This was the time we decided to go one step further. We tried to convince our good old friend JoMo from Flame Mountainboards to build a new mould and build some Decks for us and friends. Unfortunately we found out a mould which could be strong enough to give birth to a small batch was too expensive for some decks for friends. The decks would´ve been more than 400€ in retail Price if you recalculate the set up costs for 10 Decks. So JoMo couldn’t help us without charging us a big amount of cash. We thought this project will never come alive beside our 2 running prototypes :D but people kept asking for when we want to build that decks for them haha so we put our minds back together and came up with the idea of making this a community project. We found another producer which had cheaper material prices and calculated, we could afford the set up costs if we could get 30 Decks sold to the public.
+</p>
+
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/6-haero/deck2.jpg" alt="The BroModel deck">
+</p>
+
+<p>
+  So yeah that’s what we want to do now! Asking 30 of our mountainboard friends to buy one of our shred tools, to make this innovation happen. With selling 30 Decks we can afford to produce a batch of 50 Decks. Enough to hook up our new team riders with Decks and to give away some of them at the upcoming competitions. We want to achieve this Goal via Fundraising. The Riders can decide if they want this change in the Industry or not.
+  Sounds like a plan? Go ahead and support our vision! Follow our Pages <a href="https://www.facebook.com/Haeroboards">on Facebook</a> and <a href="https://www.instagram.com/haeroboards">on Instagram</a> and stay up to date, when we publish our fundraising.
+</p>
+
+<div class="embed-wrapper">
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FHaeroboards%2Fposts%2F10155644303361402%3A0&width=500" width="500" height="590" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+
+  <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FHaeroboards%2Fvideos%2F2261441523874713%2F&show_text=0&width=476" width="476" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FHaeroboards%2Fposts%2F10155657322791402%3A0&width=500" width="500" height="533" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+
+  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FHaeroboards%2Fposts%2F10155661578291402%3A0&width=500" width="500" height="595" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+
+  <p>A few of the many features detailed on <a href="https://www.facebook.com/Haeroboards/posts/">Haero Boards' Facebook page</a></p>
+</div>
+
+<p class="question">
+  This gets me excited for the next part of this interview!! Recently we have seen a rebranding of Hero Boarding. What is your new name and how did you pick it?
+</p>
+<p>
+  The new Name is Haero Boards. Sounds similar to Hero Boarding right? Haha that was on purpose. Hero Boarding made his name in mountainboarding. At least in Germany haha. I did not wanted to mix up our new project with Hero Boarding (which will still stay as my shop brand). The easiest way was to just rename my channels and keep all the fans and followers on my channel. I wanted something sounding similar.
+</p>
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/6-haero/haero-logo.jpg" alt="Haero logo">
+</p>
+
+<p class="question">
+  But what´s the meaning of Haero? Is it some greek myth figure?
+</p>
+<p>
+  Haha kind of! Haero has risen in a fun night while chatting about a good name for the upcoming brand. Earlier we talked about building aerospace related stuff like aeromobiles, because our boards are built to fly, so why not metamorphose from a Hero to Haero(mobiles) out in the Haero(space) ? Sounds logic innit?
+</p>
+
+<p class="question">
+  The decks we have seen is a 3 ply. What is your core material? And what is the reason behind 3 larger plys instead of say 6 smaller plys?
+</p>
+<p>
+  I really don’t want to get too detailed about what material we are using, but it will be a 100% European Wood!<br>
+  The reason is really easy to explain, Resin is the most heavy material in the plywood board construction. And to reduce weight it totally makes sense to reduce the glued surfaces. The best deck would be with only one Core, but that’s a real challenge, but we are on that!! Lets see how our first factory made boards will look like ;)<br>
+  Positive side effect is, that you increase the pop you can get out of the deck. Because wood have a natural tension, resin don’t. So the more pure wood, the more pop and the less weight. That easy.
+</p>
+
+<p class="question">
+  Joel has been riding this deck for a few years now. What are the main differences between what he has been riding compared to the one coming out?
+</p>
+<p>
+  Not really much, we somehow got that first shot pretty good in our eyes. We got some feedback about the Deck length, the stance preferences and the truck angles. We tried to satisfy all those preferences with our Wheelbase and Stance Options and the Wedge System without worsen the other parameters. The new deck will have a slightly deeper camber. We tested those features already. We built 3 different camber depths to find out the optimum pop and rail lock but it had to be still mellow enough to not hang up on those quarter copings while performing your beloved rock to fakie or rock n rolls. The deck will also be a bit lighter as we try the one core system and surely you always have too much resin inside if you do it DIY Style.
+</p>
+
+<p class="question">
+  Joel also rides a lot of street which can be hard on equipment that is as stiff as this deck. Did you guys run into any problems along the way and if so what have you changed to help fix that?
+</p>
+<p>
+  Surprisingly little. We´ve been really amazed, how good that deck still looks after 1 ½ years of almost daily riding in the park. We tried to build that deck as tough as possible and covered the bottom with the base and the top with the topsheet, so the fibres been protected, which seemed to pay out. The only thing happened was a scooter hitting the side of the deck so badly, that it caused the core to crack a bit. Nothing worrying, the deck is still tough, but we had to fix it with resin, to protect the core from water penetrating. After that we decided to hook up the new construction with ABS Sidewalls to make it tough enough to withstand all disasters.
+</p>
+
+<p class="question">
+  I’m sure people will be very excited to try one of these out. Does it have a name?
+</p>
+<p>
+  Yes! It will be named in honor of all the dudes helping out to make this project happen. It will be the Bro Model! Just like the first one. Because all of you guys are our bros!
+  Our sport should be all about the bros and not all about the pros like in other sports! I am really happy and honored to be a part of this very special community, we are one family!
+</p>
+
+<p class="question">
+  Now the big question. How much?
+</p>
+<p>
+  Our intention was always to get the maximum out of this project. For the rider! Therefore we also tried to keep the price as “low” as the other top brands sell their decks. We are still in calculation mode, but we will be able to stay under 300€. The first 10 backers of our fundraising will get a 20% discount, the second 30 backers will get a 10% discount. So be fast and safe some coins!
+</p>
+
+<p class="question">
+  This all sounds too good to be true! We are all very excited to hear that you are moving forward with this project. Any other secrets you might be keeping from us? Any plans to do anything more than decks?
+</p>
+<p>
+  No not really atm, the deck engineering is eating enough spare time 😉 but if somebody is asking for my help with ideas or R&D, I won't hear me say no 😉 a friend of mine is the head of skoa longboard trucks, maybe I should have a chat with him about truck design?
+</p>
+<p>
+  I think the new MBS Matrix II are a really good compromise if you are searching for a cheap and lightweight Truck. The only thing which was missing had been the hard shocks. After they came out, they are finally stiff enough to also charge BX Tracks from what I have seen. The only thing what's really annoying about them is, with the old matrix you could get cheap replacement top trucks. The new don’t? if you destroy your Top Trucks (which happens) you are forced to buy a new truck.. Why?
+  The Trampa Vertigos are still good all rounders in my eyes, but they got quite expensive lately!
+  If you are looking for a truck which is really stable almost without speed wobbles and still lightweight, I would suggest to get one of the “Handmade in Germany” Flame Dirtpipes.
+</p>
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/6-haero/flame-dirtpipes.jpg" alt="Flame mountainboard trucks">
+</p>
+<p>
+  Maybe we can chat with JoMo about a freestyle oriented truck too. The thing is, bringing out something new is really expensive so people should buy it after. If not you're bankrupt really fast. That's why we [mountainboarding] lack innovation for sure.
+</p>
+<p>
+  I think the Binding World is really up to preference, i think. I dig the MBS F4 but some are badly screaming for a MBS F3 revival. What I really do love about the F5s is the style and adjustability, but what I really dislike are the Plastic L-Brackets, they destroy every direct power transmission to the deck. I may have some ideas for really good Freeride Bindings with fixed Heel Cups, but that’s just an idea floating my mind 😊 let's see what happens after I tested them.
+</p>
+
+
+<div class="more-about">
+    More about Haero Boards:
+    <ul>
+        <li>Instagram: <a href="https://www.instagram.com/haeroboards/">https://www.instagram.com/haeroboards</a></li>
+        <li>Facebook: <a href="https://www.facebook.com/Haeroboards/">https://www.facebook.com/Haeroboards/</a></li>
+    </ul>
+</div>
+
