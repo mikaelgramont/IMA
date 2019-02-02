b93fbc888993c5ed8c94d8347bf8656119850654
Mikael Gramont <mgramont@gmail.com>
2019-02-02 09:52:16 +0100

    Added Flame interview.

---
+++ pages/interviews/7-flame-mountainboards.php
@@ -0,0 +1,139 @@
+<!-- META -->
+<meta property="og:title" content="Getting to know... Flame Mountainboards"/>
+<meta property="og:description" content="An IMA interview of Flame Mountainboards' owner, JoMo."/>
+<meta property="og:image" content="$OG_URL/images/interviews/7-flame/og.png"/>
+
+SEPARATOR
+
+<!-- SNIPPET -->
+
+<h2 class="display-font">Getting to know... Flame Mountainboards</h2>
+<p>An IMA interview of Flame Boards' owner, JoMo.</p>
+
+SEPARATOR
+
+<!-- CONTENT -->
+
+<h1 class="display-font">Getting to know... Flame Mountainboards</h1>
+
+<p>
+  There are some mountainboarders out there that haven’t heard of <a href="https://www.rideflame.de">Flame Mountainboards</a>. They do things differently and are a heavily European brand. JoMo is the the man behind their mission so we asked him some questions to further our knowledge on not only the brand but their new iteration of trucks, the Reactor.<br>
+  <i>Note: JoMo had his son answer these questions for him.</i>
+</p>
+
+<p class="question">
+  How long ago did you start Flame?
+</p>
+<p>
+  First of all JoMo (his real name is Jochen Morcher) didn't start Flame. Flame is a very old mountainboarding brand, its origins are in Bavaria in the town … right next to the alps. The founder was Albert Müller. But after some time, Flame started to fade away and was almost forgotten. JoMo was actually on the search for some better bindings when he contacted Albert. This contact lead to the adoption of Flame.
+</p>
+
+<p class="question">
+  Why did you start Flame?
+</p>
+<p>
+  Passion and the opportunity to create and improve the equipment by himself.
+</p>
+
+<p class="question">
+  What type of riding do you believe Flame Mountainboard supports? Or is it meant for everything?
+</p>
+<p>
+  Of course, it's always a question of taste which set up you use for which type of riding. Flame boards and trucks can be used for everything. From freestyle over slopestyle and boardercross, to the raw Downhill.
+</p>
+
+<p class="question">
+  Is there anyone else helping you out at Flame?
+</p>
+<p>
+  The Mountainboard Family 😉
+</p>
+
+<p class="question">
+  Where are Flame parts made?
+</p>
+<p>
+  In germany. Mainly in our basement.
+</p>
+
+<p class="question">
+  How many different designs of trucks have you had in the past?
+</p>
+<p>
+  There are two overall types. The Transporter and the Dirtpipe. The improved and new model of the Transporter is the Reactor which can be bought this year for the first time. The Dirtpipe already had two models for sale, versions 1.0 and 2.0 (the improved version). Version 3.0 is in progress.</p>
+
+<p class="question">
+  Are Flame trucks torsion trucks, cube trucks?
+</p>
+<p>
+  They are torsion Trucks , the steering is calibrated by different rubber width.
+</p>
+
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/7-flame/dirtpipe2.jpg" alt="A picture of Flame mountainboard's Dirtpipe trucks">
+  <span>The Dirtpipe truck</span>
+</p>
+
+<p class="question">
+  Do you CNC all your trucks?
+</p>
+<p>
+  Yes, in the building process of eyery truck there is CNC involved (also welding).
+</p>
+
+<p class="question">
+  What makes the Reactor trucks different from the Transporter or Dirtpipes?
+</p>
+<p>
+  As already mentioned the Transporter is kind of the older brother of the Reactor.<br>
+  So the Reactor is the improved Rransporter. Also the Reactor is something for fans who like the design. Its kind of the luxury Flame truck which you buy for the looks. Or as my dad says it: it's for the smile.<br>
+  The practical model is the Dirtpipe to which all the add ons are sold. Like the electric motors, the aggressive boarding adapter, or the hydraulic braking system.
+</p>
+
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/7-flame/reactor.png" alt="A picture of Flame mountainboard's Reactor truck">
+  <span>The Reactor truck</span>
+</p>
+
+
+<p class="question">
+  Speaking of dirtpipes. Are the Reactors a street truck and dirt pipes dirt?
+</p>
+<p>
+  No its actually the other way around. There is the aggressive boarding adapter available for the Dirtpipe which turns your normal dirtboard to a streetboard.
+</p>
+
+<p class="question">
+  You mentioned electric motors?
+</p>
+<p>
+  Yes we have been developing the system for 3 years already. Flame sells the hardware. The electrical pieces are sold by ESK8.de (close partner).
+</p>
+
+<p class="interview-image">
+  <img src="<?php echo BASE_URL ?>images/interviews/7-flame/electric.jpg" alt="A picture of Flame mountainboard's electric mountainboard setup">
+  <span>Flame's electric board setup</span>
+</p>
+
+
+<p class="question">
+  What is your dream set up? Like which deck, which trucks, hubs, and wheels?
+</p>
+<p>
+  That would be the Zorlac mega Metallica deck with dead bowl trucks and slime balls 97 A Wheels 😉
+</p>
+
+<p class="question">
+  Are you the best?
+</p>
+<p>
+  Thanks haha 🙂 We think you can only be as good as the community. So are you guys the best ?
+</p>
+
+<div class="more-about">
+    More about Flame Mountainboards:
+    <ul>
+        <li>Website: <a href="https://www.rideflame.de/">https://www.rideflame.de/</a></li>
+    </ul>
+</div>
+
