6a29fcace5fcf6485f9c7ea4873959193d8a5338
Mikael Gramont <mgramont@gmail.com>
2019-03-19 23:09:11 +0100

    Saved data into spreadsheet
    TODO:
    - backend: save JSON for debug
    - admin: display JSON data, link to spreadsheet

--- pages/wfc/paypal-transaction-complete.php
+++ pages/wfc/paypal-transaction-complete.php
@@ -1,26 +1,102 @@
 <?php
+
 use PayPalCheckoutSdk\Orders\OrdersGetRequest;
 
-$_POST = json_decode(file_get_contents('php://input'), true);
-
-$orderId = isset($_POST['orderID']) ? $_POST['orderID'] : null;
-$client = PayPalClient::client();
-$response = $client->execute(new OrdersGetRequest($orderId));
-
-$output = new stdClass();
-if ($response->result->status === 'COMPLETED') {
-  $output->status = 'OK';
-} else {
+function failWithMsg($msg)
+{
+  $output = new stdClass();
   $output->status = 'NOK';
+  $output->error = $msg;
+  echo json_encode($output);
+  exit();
 }
+
+function success()
+{
+  $output = new stdClass();
+  $output->status = 'OK';
+  echo json_encode($output);
+  exit();
+}
+
+require_once('RegistrationSaver.php');
+
+header('Content-Type: application/json');
+
+/***********************************************************************************
+ * Request stuff
+ **********************************************************************************/
+$_POST = json_decode(file_get_contents('php://input'), true);
+$orderId = isset($_POST['orderID']) ? $_POST['orderID'] : null;
+if (!$orderId) {
+  // Payment doesn't seem to have worked
+  failWithMsg('No order id given');
+}
+$registrarDetails = isset($_POST['registrarDetails']) ? $_POST['registrarDetails'] : null;
+if (!$registrarDetails) {
+  failWithMsg('No registrar details given');
+}
+$riderDetails = isset($_POST['riderDetails']) ? $_POST['riderDetails'] : null;
+if (!$riderDetails) {
+  failWithMsg('No rider details given');
+}
+
+// Necessary?
+$orderDetails = isset($_POST['orderDetails']) ? $_POST['orderDetails'] : null;
+
+/***********************************************************************************
+ * Paypal stuff
+ **********************************************************************************/
+$client = PayPalClient::client();
+$paypalValidationResponse = $client->execute(new OrdersGetRequest($orderId));
+if ($paypalValidationResponse->result->status !== 'COMPLETED') {
+  // Payment doesn't seem to have worked
+  failWithMsg('Payment not found');
+}
+
+/***********************************************************************************
+ * Spreadsheet stuff
+ **********************************************************************************/
+$errorMessage = "";
+if (!file_exists(CREDENTIALS_PATH)) {
+  $errorMessage = "No token file";
+} else {
+  try {
+    $accessToken = json_decode(file_get_contents(CREDENTIALS_PATH), true);
+  } catch (Exception $e) {
+    $errorMessage = "Could not decode the token";
+  }
+}
+if ($errorMessage) {
+  failWithMsg($errorMessage);
+}
+
+$client = Helpers::getGoogleClientForWeb($accessToken);
+$sheetsService = new Google_Service_Sheets($client);
+$logger = new Logger();
+$spreadsheetId = REGISTRATIONS_SPREADSHEET_ID;
 
 // TODO: append raw data to a json file
 
-// TODO: store filtered data to a spreadsheet
+try {
+  RegistrationSaver::save(
+    $sheetsService,
+    $logger,
+    $spreadsheetId,
+    $paypalValidationResponse->result,
+    $registrarDetails,
+    $riderDetails
+  );
+} catch (Google_Service_Exception $e) {
+  failWithMsg($e->getMessage());
+}
 
 // TODO: send an email?
 
-header('Content-Type: application/json');
-echo json_encode($output);
+// All good!
+success();
+
+
+
 
 
---
+++ php/RegistrationSaver.php
@@ -0,0 +1,73 @@
+<?php
+
+class RegistrationSaver
+{
+  const YES = 'Yes';
+  const NO = 'No';
+
+  public static function save(
+    $sheetsService, $logger, $spreadsheetId, $paypalValidationResponse, $registrarDetails, $riderDetails)
+  {
+    $logger->log("Updating row in Google Sheets.\n");
+
+    list($riderValues, $paymentValues) = self::_getSpreadSheetValues($paypalValidationResponse, $registrarDetails, $riderDetails);
+
+    $params = array("valueInputOption" => "RAW");
+    $riderRequestBody = new Google_Service_Sheets_ValueRange([
+      'values' => $riderValues
+    ]);
+    $spreadsheetResponse = $sheetsService->spreadsheets_values->append($spreadsheetId, REGISTRATIONS_SPREADSHEET_RIDER_SHEET, $riderRequestBody, $params);
+    $logger->log(sprintf("%d cells appended for rider values.", $spreadsheetResponse->getUpdates()->getUpdatedCells()));
+    $logger->log(var_export($spreadsheetResponse, true));
+
+    $paymentRequestBody = new Google_Service_Sheets_ValueRange([
+      'values' => $paymentValues
+    ]);
+    $spreadsheetResponse = $sheetsService->spreadsheets_values->append($spreadsheetId, REGISTRATIONS_SPREADSHEET_PAYMENT_SHEET, $paymentRequestBody, $params);
+    $logger->log(sprintf("%d cells appended for payment values.", $spreadsheetResponse->getUpdates()->getUpdatedCells()));
+    $logger->log(var_export($spreadsheetResponse, true));
+    error_log($logger->dumpText());
+  }
+
+  private static function _getSpreadSheetValues($paypal, $registrar, $riders)
+  {
+    $date = date('Y-m-d H:i:s');
+
+    $orderId = $paypal->id;
+    $paymentDate = $paypal->create_time;
+    $paymentAmount = $paypal->purchase_units[0]->amount->value;
+    $paymentCurrency = $paypal->purchase_units[0]->amount->currency_code;
+    $paymentName = $paypal->payer->name->given_name . ' ' . $paypal->payer->name->surname;
+    $paymentEmail = $paypal->payer->email_address;
+
+    $riderValues = array();
+    foreach ($riders as $rider) {
+      $riderValues[] = array(
+        $date,
+        $orderId,
+        $rider['firstName'],
+        $rider['lastName'],
+        $rider['category'],
+        isset($rider['slalom']) ? self::YES : self::NO,
+        isset($rider['freestyle']) ? self::YES : self::NO
+      );
+    }
+    $paymentValues = array(
+      array(
+        $date,
+        $orderId,
+        $paymentName,
+        $paymentEmail,
+        $paymentDate,
+        $paymentAmount,
+        $paymentCurrency,
+        sizeof($riders),
+        $registrar['firstName'],
+        $registrar['lastName'],
+        $registrar['email']
+      )
+    );
+
+    return array($riderValues, $paymentValues);
+  }
+}
