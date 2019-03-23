986d4258031f0cb6cd9dc1db2ba875aa1dd4fcbe
Mikael Gramont <mgramont@gmail.com>
2019-03-23 09:58:49 +0100

    Removing error display

--- wfc/index.php
+++ wfc/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
 ini_set('xdebug.default_enable', false);
 ini_set('xdebug.html_errors', false);
 
