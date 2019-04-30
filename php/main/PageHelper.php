0de165e9ea5f584bf9db02f3f2bdacb2a93e1f94
Mikael Gramont <mgramont@gmail.com>
2019-04-30 13:26:53 +0200

    USE_HTTPS constant specifies whether to use https in urls.

--- php/main/PageHelper.php
+++ php/main/PageHelper.php
@@ -61,6 +61,9 @@ HTML;
   {
     if (isset($_SERVER['HTTP_HOST'])) {
       $http = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+      if (USE_HTTPS) {
+        $http = 'https';
+      }
       $hostname = $_SERVER['HTTP_HOST'];
       $dir = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
