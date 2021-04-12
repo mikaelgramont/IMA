06bc0e1ac6dc0c2430496d3d69158aa491317238
Mikael Gramont <mgramont@gmail.com>
2021-04-12 08:44:56 +0200

    Added 2021 WMC announcement

---
+++ pages/main/news/29-2021-wmc-cancelled.php
@@ -0,0 +1,110 @@
+<style>
+  .video {
+    display: block;
+    margin: 10px auto;
+    max-width: 560px;
+    width: 100%;
+    height: 315px;
+  }
+  .photo {
+    max-width: 100%;
+    margin: .5em 0;
+  }
+  .announcement {
+    max-width: 700px;
+    margin: 1em auto;
+  }
+  .details {
+    max-width: 700px;
+    margin: 1em auto;
+  }
+  dt {
+    font-weight: bold;
+  }
+  .map {
+    width: 600px;
+    height: 450px;
+  }
+  @media screen and (max-width: 640px) {
+    .map {
+      width: 80vw;
+      height: 80vw;
+    }
+  }
+</style>
+
+<section>
+  <p>
+    Dear mountainboard community, it is hard to imagine that it has been a year since our last announcement. No one
+    could have seen the pandemic coming and there was no way to prepare. But it has been good to see everyone taking
+    care of themselves and staying healthy during these troubling times. Even though the community has been kept apart
+    physically, we have united on social media, stayed tight, and had some sessions with each other. Unfortunately as we
+    edge closer to this year’s World Championships we have an announcement to make.
+  </p>
+
+  <h1 class="display-font">The 2021 World Mountainboard Championship is cancelled</h1>
+  <p>
+    It is with heavy hearts that the International Mountainboard Association in conjunction with the Association
+    Mountainboard Compiegne have made the decision to postpone the 2021 Championship.
+  </p>
+  <p>
+    There were many talks with each other regarding this decision and it was a tough call to make.
+  </p>
+  <p>
+    Many of the concerns raised last year are the same reasons we have come to this conclusion. Such as travel
+    restrictions, lockdowns, financial burdens on athletes, and in turn a nominal turnout for a competition known to
+    draw the highest number of competitors.
+  </p>
+
+  <h1 class="display-font">The Good News: there will still be riding</h1>
+  <p>
+    Look for the teams in France to put on gatherings. It’s not that we didn’t think anything should happen on these
+    dates, only that the World Championships was not the best choice. The good news is that to make up for this, the
+    teams at AMC and at LA Mountainboard are preparing a low key, week-long mountainboard event at their respective
+    parks.
+  </p>
+  <p>
+    So for those who were looking forward to riding the epic Compiegne park, you can still do so, and also check out the
+    amazing LA Mountainboard park in Rouans (near Nantes)! Full details will be released soon but the rough idea is the
+    following :
+  </p>
+  <ul>
+    <li>
+      July 10th-13th: <a href="https://www.youtube.com/watch?v=LjqbyX2QrNg">LA Mountainboard park</a>,
+      <a href="https://www.youtube.com/watch?v=6keieQRNh7k">Nantes skatepark</a>,
+      <a href="https://www.youtube.com/watch?v=h_56YxcTc14">Abbaretz open mine</a>
+    </li>
+    <li>
+      July 14th: on the road to Compiegne
+    </li>
+    <li>
+      July 15th-18th: riding in Compiegne (their park, nearby forest tracks, and other things)
+    </li>
+  </ul>
+  <p>
+    If you've been to either or both of these parks, you'll be happy to hear that they have both undergone large amounts
+    of work, and there will be new things to ride!
+  </p>
+
+  <h1 class="display-font">More Good News: the 4 Down Project is still on!</h1>
+  <p>
+    That's right! Another 4 teams have been selected to participate in this year's annual video contest.
+    More details will be released at the beginning of May but know that the teams in for this year will make it a fun
+    one!
+  </p>
+
+  <h1 class="display-font">Looking To The Future!</h1>
+  <p>
+    We know there will be questions about what happens next. There will be a flip flop in events. Australia will hold
+    the World Championships in 2022 and France will be the home of the Championship in 2023.
+  </p>
+  <p>
+    Australia will be coming off a hot year of building 2 new public parks that all athletes that join will have the
+    chance to ride. They will also hold their competition in late 2022 giving us all more time to save up and make
+    travel plans.
+  </p>
+  <p>
+    Many thanks to Ludo and the crew at Association Mountainboard Compiegne. They have been on a roller coaster ride
+    with Covid19 and planning this contest. Thank you for being understanding and fluid to make all of this work.
+  </p>
+</section>
